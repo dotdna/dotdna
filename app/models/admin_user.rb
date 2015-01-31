bc64d23c97b72b4ef8ed82d8de265df55c747b6d
Teddy <teddya2007@gmail.com>
2015-01-31 13:37:08 +0200

    Authentication Plugin

---
+++ app/models/admin_user.rb
@@ -0,0 +1,9 @@
+class AdminUser < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :rememberable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+end
