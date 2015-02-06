e326be3706e431ac9056b74b4f642f8b9f904aa1
Teddy <teddya2007@gmail.com>
2015-02-06 20:36:24 +0200

    Stock

--- app/controllers/fitment_center_stocks_controller.rb
+++ app/controllers/fitment_center_stocks_controller.rb
@@ -1,6 +1,13 @@
 class FitmentCenterStocksController < ApplicationController
   # GET /fitment_center_stocks
   # GET /fitment_center_stocks.json
+
+  before_filter :authenticate_fitment_center_user!
+
+  skip_before_filter :authenticate_admin_user!
+
+  layout 'fitment_center_layout'
+
   def index
     @fitment_center_stocks = FitmentCenterStock.all
 
