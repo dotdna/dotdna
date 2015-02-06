9d25b7c71cc9f5a8eaa050bcd6c8f95b7309aaf9
Teddy <teddya2007@gmail.com>
2015-02-06 20:23:51 +0200

    Add Skip

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,13 @@
 class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
+
+  before_filter :authenticate_fitment_center_user!
+
+  skip_before_filter :authenticate_admin_user!
+
+  layout 'fitment_center_layout'
+
   def index
     @orders = Order.all
 
